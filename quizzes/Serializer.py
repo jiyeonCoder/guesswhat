318d8caf72bc2f91865d517699515c82bb3abe91
장원\jjw95 <jjw951228@naver.com>
2023-10-23 17:06:33 +0900

    comment 추가중

---
+++ quizzes/Serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    nickname = serializers.SerializerMethodField()
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+    def get_nickname(self, obj):
+        return obj.user_id.nickname
